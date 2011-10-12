7bd5374742886025a3fa127fae131c6f5e27dee5
Tom Barber <tom@analytical-labs.com>
2011-10-12 21:00:50 +0100

    fix typo

--- manifests/classes/virtualmachines.pp
+++ manifests/classes/virtualmachines.pp
@@ -3,7 +3,7 @@ class analyticallabs::virtualmachines{
   xen::guest { "dev2.analytical-labs.com":
     ensure => "running",
     disksize => '100G',
-    ram => '4096',
+    ram => '4096M',
     vcpus => 2,
     ipaddr => '85.10.241.74'
   }
