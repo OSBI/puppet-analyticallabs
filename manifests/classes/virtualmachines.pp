108c79ed37552d779ca8018a19c1e55ce61bd218
Tom Barber <tom@analytical-labs.com>
2011-10-13 12:04:35 +0100

    add webserver2 node

--- manifests/classes/virtualmachines.pp
+++ manifests/classes/virtualmachines.pp
@@ -8,6 +8,12 @@ class analyticallabs::virtualmachines{
     ipaddr => '85.10.241.74'
   }
   
-  
+  xen::guest { "webserver2.analytical-labs.com":
+  	ensure => "running",
+  	disksize => "100G",
+  	ram => "4096M",
+  	vcpus => 2,
+  	ipaddr => '85.10.241.75'
+  }
 
 }
