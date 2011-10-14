1bbe157aaba10d5d573a1dc2c9bf79a50e100e66
Tom Barber <tom@analytical-labs.com>
2011-10-14 11:55:50 +0100

    changes to extdata£

---
+++ manifests/classes/artifactory.pp
@@ -0,0 +1,16 @@
+class analyticallabs::artifactory {
+
+	include artifactory
+	
+	$artifactory_http			= extlookup('artifactory_http','')
+	$artifactory_ajp			= extlookup('artifactory_ajp','')
+	$artifactory_server		= extlookup('artifactory_server','')
+
+	artifactory::instance{ "repo":
+		ensure => present
+		artifactory_ajp => $artifactory_http
+		artifactory_http => $artifactory_ajp
+		artifactory_server => $artifactory_server
+	}
+
+}
