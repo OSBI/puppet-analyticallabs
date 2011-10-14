718873bbeff6d5568561e2d49a47dfbdc0f86c67
Tom Barber <tom@analytical-labs.com>
2011-10-14 09:52:26 +0100

    alabs

--- manifests/classes/saikucontrol.pp
+++ manifests/classes/saikucontrol.pp
@@ -14,5 +14,16 @@ class analyticallabs::saiku::control {
   		vhost    => "demo.analytical-labs.com",
   		url      => "ajp://localhost:8009/",
 	}
-
+	
+	apache::vhost {"dev.analytical-labs.com":
+  		ensure => present,
+	}
+	
+	apache::proxypass {"devsaiku":
+  		ensure   => present,
+  		location => "/",
+  		vhost    => "dev.analytical-labs.com",
+  		url      => "ajp://localhost:8011/",
+	}
+	
 }
