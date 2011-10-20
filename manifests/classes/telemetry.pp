79bdc593e042da10916f92a0d6d3362905bf8113
Tom Barber <tom@analytical-labs.com>
2011-10-20 16:26:31 +0100

    telemetry2

---
+++ manifests/classes/telemetry.pp
@@ -0,0 +1,11 @@
+analyticallabs::telemetry {
+	include nodejs	
+	exec { "install telemetry":
+    	command => "npm install -g telemetry",
+    	creates => "/usr/bin/telemetry",
+    }
+
+    file { "/usr/lib/node_modules/telemetry/config.yaml" :
+        content => template("analyticallabs/telemetry_config.yaml.erb"),
+    }
+}
