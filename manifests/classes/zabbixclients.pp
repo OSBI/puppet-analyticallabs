11482f2dc88ebbf7c3d89a3a0bfa1739ee3dde59
Tom Barber <tom@analytical-labs.com>
2011-10-22 00:18:23 +0100

    new monitoring ip

--- manifests/classes/zabbixclients.pp
+++ manifests/classes/zabbixclients.pp
@@ -1,6 +1,6 @@
 class analyticallabs::zabbix::client {
     $shortname = "${fqdn}" # the name Zabbix Server uses for this node
-    $zabbix_server = "46.4.83.44"
+    $zabbix_server = "46.137.95.179"
 
 	include zabbix::agent    
      
