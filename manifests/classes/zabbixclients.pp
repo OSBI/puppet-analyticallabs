ae0235a4c762f5f6693d5fe20aae55270f343f5a
Tom Barber <tom@analytical-labs.com>
2011-10-21 22:39:19 +0100

    swap around2

--- manifests/classes/zabbixclients.pp
+++ manifests/classes/zabbixclients.pp
@@ -1,9 +1,8 @@
 class analyticallabs::zabbix::client {
-
-include zabbix::agent
     $shortname = "${fqdn}" # the name Zabbix Server uses for this node
     $zabbix_server = "46.4.83.44"
-    
+
+	include zabbix::agent    
      
 
 }
