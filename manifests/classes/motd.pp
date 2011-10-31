48227a91ca77ab817f6571c7b9d0c1108befa0ad
Tom Barber <tom@analytical-labs.com>
2011-10-31 14:56:37 +0000

    add param

--- manifests/classes/motd.pp
+++ manifests/classes/motd.pp
@@ -1,5 +1,8 @@
 class analyticallabs::motd {
   include motd
+
+  $puppet_server      = extlookup('puppet_server'      ,'puppet')
+
   motd::message { "motd":
        source => template('analyticallabs/system.motd.erb') } 
   
