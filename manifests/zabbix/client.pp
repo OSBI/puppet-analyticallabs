d5a057068732073384cafdce0249a7082c7949f9
Tom Barber <tom@analytical-labs.com>
2012-03-21 22:52:16 +0000

    fix variable

--- manifests/zabbix/client.pp
+++ manifests/zabbix/client.pp
@@ -1,5 +1,5 @@
 class analyticallabs::zabbix::client {
-  $shortname = "${fqdn}" # the name Zabbix Server uses for this node
+  $shortname = "${::fqdn}" # the name Zabbix Server uses for this node
   $zabbix_server = "176.34.241.162"
 
   include zabbix::agent    
