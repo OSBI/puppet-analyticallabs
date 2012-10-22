480eeec928f64b5be356d055171a38e2411e47e2
Tom Barber <tom@analytical-labs.com>
2012-10-22 15:26:19 +0100

    projects

--- manifests/websites/projects.pp
+++ manifests/websites/projects.pp
@@ -12,9 +12,33 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class analyticallabs::websites::projects {
+	$content = "<VirtualHost *:80>
+  ServerName projects.analytical-labs.com
+
+
+  DocumentRoot /var/www/projects.analytical-labs.com/htdocs/redmine
+  PassengerResolveSymlinksInDocumentRoot on
+Options Indexes ExecCGI FollowSymlinks
+  LogLevel warn
+
+  ErrorLog /var/www/projects.analytical-labs.com/logs/error.log
+  CustomLog /var/www/projects.analytical-labs.com/logs/access.log \"combined\"
+
+  Include /var/www/projects.analytical-labs.com/conf/*.conf
+
+  ScriptAlias /cgi-bin/ /var/www/projects.analytical-labs.com/cgi-bin/
+  <Directory /var/www/projects.analytical-labs.com/cgi-bin/>
+    Options +ExecCGI
+    AddHandler cgi-script .cgi
+  </Directory>
+
+  ServerSignature Off
+
+</VirtualHost>" 
 	apache::vhost {
 		"projects.analytical-labs.com" :
 			ensure => present,
+			config_content => $content,
 }
 }
 
