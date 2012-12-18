2ba0fc729f242bfb07d73d505415986069012888
Tom Barber <tom@analytical-labs.com>
2012-12-18 21:43:06 +0000

    refactor

--- manifests/backup/configure.pp
+++ manifests/backup/configure.pp
@@ -2,10 +2,6 @@
 
 class analyticallabs::backup::configure {
 
-  $ec2id = extlookup('ec2id', '')
-$ec2key = extlookup('ec2key', '')
-$s3destination = extlookup('s3destination', '')
-
 if($::hostname == "vhost"){
   class { "mysql::backup":}
 $backupfilelist = "/var/www/
